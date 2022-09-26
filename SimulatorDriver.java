81bfd9f7b2d0d9261aebbb25c8921f1117c74e2a
Eric Huang <ERICHUANG54@GMAIL.COM>
2022-09-26 13:58:45 -0700

    testing Student class

--- SimulatorDriver.java
+++ SimulatorDriver.java
@@ -1,3 +1,10 @@
 public class SimulatorDriver {
-    
+    public static void main(String[] args)
+    {
+        Student Student1 = new Student();
+        Student Student2 = new Student();
+
+        System.out.println(Student1.getID());
+        System.out.println(Student2.getID());
+    }
 }
