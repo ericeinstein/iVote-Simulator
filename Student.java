7b00c22dc3eb1022e0033a407d922ddd87080853
Eric Huang <ERICHUANG54@GMAIL.COM>
2022-09-26 13:57:48 -0700

    created ID generator

--- Student.java
+++ Student.java
@@ -1,3 +1,20 @@
+import java.util.Random;
 public class Student {
-    
+    Random rand = new Random();
+    private String ID = "014"; //all ID's start with 014
+    public Student()
+    {
+        setID();
+    }
+
+    public void setID()
+    {
+        for (int i = 0; i < 4; i++)
+            ID += rand.nextInt(10); //last 4 digits are random
+    }
+
+    public String getID()
+    {
+        return ID;
+    }
 }
