ec96463064df7cbce18e2997b3378f7bcfc4be78
Eric Huang <ERICHUANG54@GMAIL.COM>
2022-09-28 15:44:33 -0700

    changed to interface

--- Question.java
+++ Question.java
@@ -1,68 +1,11 @@
-public class Question {
-    private String question;
-    private String choice1;
-    private String choice2;
-    private String choice3;
-    private String choice4;
-    private int answer;
+interface Question {
+    public String getQuestion();
 
-    public void setQuestion(String inputQuestion)
-    {
-        question = inputQuestion;
-    }
+    public void setChoice(String inputAnswer, int choiceNum);
 
-    public String getQuestion()
-    {
-        return question;
-    }
+    public String getChoice(int choiceNum);
 
-    public void setAnswer1(String inputAnswer1)
-    {
-        choice1 = inputAnswer1;
-    }
+    public void setAnswer(int inputAnswer);
 
-    public String getAnswer1()
-    {
-        return choice1;
-    }
-
-    public void setAnswer2(String inputAnswer2)
-    {
-        choice2 = inputAnswer2;
-    }
-
-    public String getAnswer2()
-    {
-        return choice2;
-    }
-
-    public void setAnswer3(String inputAnswer3)
-    {
-        choice3 = inputAnswer3;
-    }
-
-    public String getAnswer3()
-    {
-        return choice3;
-    }
-
-    public void setAnswer4(String inputAnswer4)
-    {
-        choice4 = inputAnswer4;
-    }
-
-    public String getAnswer4()
-    {
-        return choice4;
-    }
-
-    public void setAnswer(int inputAnswer)
-    {
-        answer = inputAnswer;
-    }
-
-    public int getAnswer()
-    {
-        return answer;
-    }
+    public int getAnswer();
 }
