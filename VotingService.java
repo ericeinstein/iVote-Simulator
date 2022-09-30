8c6a5d57a3f04b4bc13193f7c0b1f6961f69ed54
Eric Huang <ERICHUANG54@GMAIL.COM>
2022-09-29 23:06:50 -0700

    implemented voting service

--- VotingService.java
+++ VotingService.java
@@ -1,6 +1,33 @@
+import java.util.Random;
+
 public class VotingService {
-    public Student[] collectAnswers(Student[] studentArray)
+
+    //collects and prints out results
+    public static Student[] collectAnswers(Student[] studentArray, int typeOfQuestion)
     {
+        Random rand = new Random();
+        int length = studentArray.length;
+        //case if multiple choice question
+        if (typeOfQuestion == 1)
+        {
+            int numChoice1 = rand.nextInt(length);
+            int numChoice2 = rand.nextInt(length - numChoice1);
+            int numChoice3 = rand.nextInt(length - (numChoice1 + numChoice2));
+            int numChoice4 = length - (numChoice1 + numChoice2 + numChoice3);
+            System.out.println(numChoice1 + " students chose 1");
+            System.out.println(numChoice2 + " students chose 2");
+            System.out.println(numChoice3 + " students chose 3");
+            System.out.println(numChoice4 + " students chose 4");
+        }
+        //case if true/false question
+        else if (typeOfQuestion == 2)
+        {
+            int numChoice1 = rand.nextInt(length);
+            int numChoice2 = length - numChoice1;
+            System.out.println(numChoice1 + " students chose 1");
+            System.out.println(numChoice2 + " students chose 2");
+        }
+
         return studentArray;
     }
 }
