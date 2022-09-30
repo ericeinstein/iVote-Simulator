c388763cd6414d7fbb1abcca5161220b35334237
Eric Huang <ERICHUANG54@GMAIL.COM>
2022-09-29 23:06:36 -0700

    finalized simulation

--- SimulatorDriver.java
+++ SimulatorDriver.java
@@ -5,16 +5,45 @@ import java.util.Timer;
 public class SimulatorDriver {
     public static void main(String[] args)
     {
-        //creates 2 multiple choice questions and 2 true/false questions
-        MultipleChoiceQuestion question1 = new MultipleChoiceQuestion("Who was the second President of the United States?");
-        question1.setChoice("Andrew Jackson", 1);
-        question1.setChoice("Thomas Jefferson", 2);
-        question1.setChoice("John Adams", 3);
-        question1.setChoice("Zachary Taylor", 4);
-        question1.setAnswer(3);
+        Random rand = new Random();
+        int numOfStudents = rand.nextInt(10) + 10;
+        Student[] studentArray = new Student[numOfStudents];
+        for (int i = 0 ; i < numOfStudents ; i++)
+            studentArray[i] = new Student();
+        System.out.println("Number of students in this class: " + studentArray.length);
+        
 
-        Timer t = new java.util.Timer();
-        t.schedule( 
+        //creates 2 multiple choice questions and 2 true/false questions
+        Timer t0 = new java.util.Timer();
+        t0.schedule( 
+        new java.util.TimerTask() {
+            @Override
+            public void run() {
+                MultipleChoiceQuestion question1 = new MultipleChoiceQuestion("Who was the second President of the United States?");
+                question1.setChoice("Andrew Jackson", 1);
+                question1.setChoice("Thomas Jefferson", 2);
+                question1.setChoice("John Adams", 3);
+                question1.setChoice("Zachary Taylor", 4);
+                question1.setAnswer(3);
+                Timer t0a = new java.util.Timer();
+                t0a.schedule( 
+                new java.util.TimerTask() {
+                    @Override
+                    public void run() {
+                        VotingService.collectAnswers(studentArray, 1);
+                        t0a.cancel();
+                    }
+                }, 
+                1000
+                );
+                t0.cancel();
+            }
+        }, 
+        1000
+        );
+
+        Timer t1 = new java.util.Timer();
+        t1.schedule( 
         new java.util.TimerTask() {
             @Override
             public void run() {
@@ -24,11 +53,23 @@ public class SimulatorDriver {
                 question2.setChoice("1954", 3);
                 question2.setChoice("1919", 4);
                 question2.setAnswer(1);
-                t.cancel();
+                Timer t1a = new java.util.Timer();
+                t1a.schedule( 
+                new java.util.TimerTask() {
+                    @Override
+                    public void run() {
+                        VotingService.collectAnswers(studentArray, 1);
+                        t1a.cancel();
+                    }
+                }, 
+                1000
+                );
+                t1.cancel();
             }
         }, 
-        1000
+        4000
         );
+
 
         Timer t2 = new java.util.Timer();
         t2.schedule( 
@@ -39,11 +80,23 @@ public class SimulatorDriver {
                 question3.setChoice("True", 1);
                 question3.setChoice("False", 2);
                 question3.setAnswer(1);
+                Timer t2a = new java.util.Timer();
+                t2a.schedule( 
+                new java.util.TimerTask() {
+                    @Override
+                    public void run() {
+                        VotingService.collectAnswers(studentArray, 2);
+                        t2a.cancel();
+                    }
+                }, 
+                1000
+                );
                 t2.cancel();
             }
         }, 
-        2000 
+        7000
         );
+
 
         Timer t3 = new java.util.Timer();
         t3.schedule( 
@@ -54,12 +107,21 @@ public class SimulatorDriver {
                 question3.setChoice("True", 1);
                 question3.setChoice("False", 2);
                 question3.setAnswer(1);
+                Timer t3a = new java.util.Timer();
+                t3a.schedule( 
+                new java.util.TimerTask() {
+                    @Override
+                    public void run() {
+                        VotingService.collectAnswers(studentArray, 2);
+                        t3a.cancel();
+                    }
+                }, 
+                1000
+                );
                 t3.cancel();
             }
         }, 
-        3000 
+        10000 
         );
-        
-        
     }
 }
